41f7140191886161cd6744067732cd101dbf7d9a
Shane Knight <s3785357@student.rmit.edu.au>
2026-04-07 17:25:48 +1000

    add database functionality to  sensormonitor

--- Assignment1/Task3/SensorMonitor.py
+++ Assignment1/Task3/SensorMonitor.py
@@ -5,6 +5,7 @@ except ImportError:
 
 import time
 import json
+import sqlite3
 
 sense = SenseHat()
 
@@ -12,8 +13,47 @@ sense = SenseHat()
 with open("enviro_config.json") as f:
     thresholds = json.load(f)
 
-TEMP_OFFSET   = 2.0
+TEMP_OFFSET = 2.0
 POLL_INTERVAL = 10
+DB_NAME = "envirotrack.db"
+
+# colours for LED display
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+AMBER = (255, 165, 0)
+
+def get_status_colour(status):
+    if status == "Comfortable" or status == "Aligned":
+        return GREEN
+    elif status == "High":
+        return RED
+    elif status == "Low":
+        return BLUE
+    else:
+        return AMBER
+
+# create the database and table if they dont already exist
+def create_db():
+    con = sqlite3.connect(DB_NAME)
+    with con:
+        con.execute("""
+            CREATE TABLE IF NOT EXISTS readings (
+                timestamp DATETIME DEFAULT (datetime('now')),
+                temperature REAL,
+                temp_status TEXT,
+                humidity REAL,
+                hum_status TEXT,
+                pressure REAL,
+                pres_status TEXT,
+                pitch REAL,
+                pitch_status TEXT,
+                roll REAL,
+                roll_status TEXT,
+                yaw REAL,
+                yaw_status TEXT
+            )
+        """)
 
 def classify_environment(reading, threshold):
     if reading < threshold["min"]:
@@ -30,33 +70,77 @@ def classify_orientation(angle, threshold):
         return "Tilted"
 
 def read_sensors():
+    # offset tempt
     temperature = round(sense.get_temperature() - TEMP_OFFSET, 2)
-    humidity    = round(sense.get_humidity(), 2)
-    pressure    = round(sense.get_pressure(), 2)
+    humidity = round(sense.get_humidity(), 2)
+    pressure = round(sense.get_pressure(), 2)
 
     orientation = sense.get_orientation()
     pitch = orientation["pitch"]
-    roll  = orientation["roll"]
-    yaw   = orientation["yaw"]
+    roll = orientation["roll"]
+    yaw = orientation["yaw"]
 
+    # change pitch and roll from 0-360 to -180 to 180
     if pitch > 180:
         pitch -= 360
     if roll > 180:
         roll -= 360
 
     pitch = round(pitch, 2)
-    roll  = round(roll,  2)
-    yaw   = round(yaw,   2)
+    roll = round(roll, 2)
+    yaw = round(yaw, 2)
 
-    # print all readings with their status
+    # store each reading as touple
+    sensor_data = {
+        "temperature": (temperature, classify_environment(temperature, thresholds["temperature"])),
+        "humidity": (humidity, classify_environment(humidity, thresholds["humidity"])),
+        "pressure": (pressure, classify_environment(pressure, thresholds["pressure"])),
+        "pitch": (pitch, classify_orientation(pitch, thresholds["orientation"]["pitch"])),
+        "roll": (roll, classify_orientation(roll, thresholds["orientation"]["roll"])),
+        "yaw": (yaw, classify_orientation(yaw, thresholds["orientation"]["yaw"])),
+    }
+
+    # save reading to the database
+    con = sqlite3.connect(DB_NAME)
+    con.execute(
+        "INSERT INTO readings (temperature, temp_status, humidity, hum_status, pressure, pres_status, pitch, pitch_status, roll, roll_status, yaw, yaw_status) VALUES (" +
+        str(temperature) + ", '" + sensor_data["temperature"][1] + "', " +
+        str(humidity) + ", '" + sensor_data["humidity"][1] + "', " +
+        str(pressure) + ", '" + sensor_data["pressure"][1] + "', " +
+        str(pitch) + ", '" + sensor_data["pitch"][1] + "', " +
+        str(roll) + ", '" + sensor_data["roll"][1] + "', " +
+        str(yaw) + ", '" + sensor_data["yaw"][1] + "')"
+    )
+    con.commit()
+    con.close()
+
     print(time.strftime("%H:%M:%S"))
-    print("  temperature: " + str(temperature) + "  [" + classify_environment(temperature, thresholds["temperature"]) + "]")
-    print("  humidity:    " + str(humidity)    + "  [" + classify_environment(humidity,    thresholds["humidity"])    + "]")
-    print("  pressure:    " + str(pressure)    + "  [" + classify_environment(pressure,    thresholds["pressure"])    + "]")
-    print("  pitch:       " + str(pitch)       + "  [" + classify_orientation(pitch, thresholds["orientation"]["pitch"]) + "]")
-    print("  roll:        " + str(roll)        + "  [" + classify_orientation(roll,  thresholds["orientation"]["roll"])  + "]")
-    print("  yaw:         " + str(yaw)         + "  [" + classify_orientation(yaw,   thresholds["orientation"]["yaw"])   + "]")
+    print("  temperature: " + str(temperature) + "  [" + sensor_data["temperature"][1] + "]")
+    print("  humidity: " + str(humidity) + "  [" + sensor_data["humidity"][1] + "]")
+    print("  pressure: " + str(pressure) + "  [" + sensor_data["pressure"][1] + "]")
+    print("  pitch: " + str(pitch) + "  [" + sensor_data["pitch"][1] + "]")
+    print("  roll: " + str(roll) + "  [" + sensor_data["roll"][1] + "]")
+    print("  yaw: " + str(yaw) + "  [" + sensor_data["yaw"][1] + "]")
+
+    return sensor_data
+
+def show_on_led(sensor_data):
+    displays = [
+        ("T:", "temperature"),
+        ("H:", "humidity"),
+        ("P:", "pressure"),
+        ("Pi:", "pitch"),
+        ("R:", "roll"),
+        ("Y:", "yaw"),
+    ]
+    for label, key in displays:
+        value, status = sensor_data[key]
+        colour = get_status_colour(status)
+        sense.show_message(label + str(int(round(value))), text_colour=colour, scroll_speed=0.05)
+
+# create db on startup
+create_db()
 
 while True:
-    read_sensors()
-    time.sleep(POLL_INTERVAL)
+    data = read_sensors()
+    show_on_led(data)
